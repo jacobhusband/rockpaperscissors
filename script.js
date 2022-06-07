678e438242c23842309512b9cf004dde390e0260
Jacob Husband <jakehusband2@gmail.com>
2022-06-07 10:46:30 -0700

    add UI to rock paper scissors

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const result = document.getElementById("result");
+
+rock.addEventListener("click", playRound);
+paper.addEventListener("click", playRound);
+scissors.addEventListener("click", playRound);
+
 function computerSelection() {
   let randomNum = Math.floor(Math.random() * 3 + 1);
   if (randomNum === 1) {
@@ -9,39 +20,41 @@ function computerSelection() {
   }
 }
 
-function playerSelection() {
-  let input = prompt("Rock, paper, or scissors?");
-  input = input.toLowerCase();
-  if (input === "rock") {
+function playerSelection(event) {
+  const input = event.target.getAttributeNames()[4];
+  if (input === "data-rock") {
     return "rock";
-  } else if (input === "paper") {
+  } else if (input === "data-paper") {
     return "paper";
-  } else if (input === "scissors") {
+  } else if (input === "data-scissors") {
     return "scissors";
-  } else {
-    playerSelection();
   }
 }
 
-function playRound() {
-  const player = playerSelection();
+function playRound(event) {
+  const player = playerSelection(event);
   const computer = computerSelection();
   if (player === computer) {
-    console.log(`Computer and you chose ${player}, play again!`);
-    playRound();
-  } else if (player === "rock" && computer === "paper") {
-    console.log(
-      `Computer chose ${computer} and you chose ${player}, computer won!`
-    );
-  } else if (player === "rock" && computer === "scissors") {
-    console.log(`Computer chose ${computer} and you chose ${player}, you won!`);
-  } else if (computer === "rock" && player === "paper") {
-    console.log(`Computer chose ${computer} and you chose ${player}, you won!`);
-  } else if (computer === "rock" && player === "scissors") {
-    console.log(
-      `Computer chose ${computer} and you chose ${player}, computer won!`
-    );
+    result.innerHTML = "Tie";
+  } else if (
+    (player === "rock" && computer === "paper") ||
+    (player === "paper" && computer === "scissors") ||
+    (player === "scissors" && computer === "rock")
+  ) {
+    result.innerHTML = "Computer won";
+    computerScore.innerHTML = +computerScore.innerHTML + 1;
+    checkForWinner(+computerScore.innerHTML, "Computer");
+  } else {
+    result.innerHTML = "You won";
+    playerScore.innerHTML = +playerScore.innerHTML + 1;
+    checkForWinner(+playerScore.innerHTML, "Player");
   }
 }
 
-playRound();
+function checkForWinner(score, person) {
+  if (score >= 5) {
+    result.innerHTML = `${person} won the battle!`;
+    computerScore.innerHTML = 0;
+    playerScore.innerHTML = 0;
+  }
+}
